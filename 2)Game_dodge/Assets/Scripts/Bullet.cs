b6847c15b1625f2fd4559b50548516413c62ff02
LeeHann <0104ihn@naver.com>
2020-09-02 11:42:55 +0900

    0902

---
+++ 2)Game_dodge/Assets/Scripts/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bullet;
+
+    void Start()
+    {
+        bullet = GetComponent<Rigidbody>();
+        //탄알의 속도 = 앞 쪽 방향 * 이동 속력
+        bullet.velocity = transform.forward * speed;
+
+        //3초 뒤에 자신의 게임 오브젝트 파괴
+        Destroy(gameObject, 3f);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player")
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            
+            if(player != null)
+            {
+                player.Die();
+            }
+        }    
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ 2)Game_dodge/Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    // 총알이 랜덤한 주기로 생성
+    // 맞출 대상을 향해 총알 발사
+
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f; //최소 총알 생성 주기
+    public float spawnRateMax = 3f; //최대 생성 주기
+
+    private Transform target; // 대상의 위치
+    private float spawnRate; // 생성 주기
+    private float timeAfterSpawn; //최근 생성 이후 흐른 시간
+
+    void Start()
+    {
+        target = FindObjectOfType<PlayerController>().transform;
+        SetRandomSpawnRate();
+    }
+
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+
+        if (timeAfterSpawn >= spawnRate)
+        {
+            GameObject newObj = Instantiate(bulletPrefab, transform.position, transform.rotation); // 총알 생성  
+            
+            newObj.transform.LookAt(target);
+
+            timeAfterSpawn = 0f;
+            SetRandomSpawnRate();
+        }
+    }
+    void SetRandomSpawnRate()
+    {
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+    }
+}
