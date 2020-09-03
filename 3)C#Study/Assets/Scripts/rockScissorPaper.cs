bc066f7d60eb79005d3e4a8c58f824ee59227b6f
LeeHann <0104ihn@naver.com>
2020-09-03 17:51:03 +0900

    0903

--- 3)C#Study/Assets/Scripts/rockScissorPaper.cs
+++ 3)C#Study/Assets/Scripts/rockScissorPaper.cs
@@ -15,51 +15,103 @@ public class rockScissorPaper : MonoBehaviour
 
     public enum GameResult
     {
-        Win,
-        Lose,
-        Draw
+        Win,Lose,Draw
     }
 
     // 0: 가위, 1: 바위, 2: 보
     public Hand player; // 플레이어가 낸 가위바위보 값
-    public Hand pc; // 컴퓨터가 낸 가위바위보 값
+    public Hand pc; // 컴퓨터가 낸 가위바위보 값, enum 타입
+    public Hand[] pcs; // array : 인스턴스 타입
+    public int numOfPcs = 10;
 
-    void Start()
+    // public GameResult playerOne;
+    // public GameResult playerTwo;
+
+    private void Start() 
     {
-        
+        pcs = new Hand[numOfPcs];
     }
 
     private void OnGUI() 
     {
         if(GUI.Button(new Rect(10,10,300,100), "가위"))
         {
-            StratGame(Hand.scissor);
+            StartGame(Hand.scissor, pc);
         }
-        if(GUI.Button(new Rect(10,110,300,100), "바위"))
+        if(GUI.Button(new Rect(10,120,300,100), "바위"))
         {
-            StratGame(Hand.rock);
+            StartGame(Hand.rock, pc);
         }
-        if(GUI.Button(new Rect(10,220,300,100), "보"))
+        if(GUI.Button(new Rect(10,230,300,100), "보"))
         {
-            StratGame(Hand.paper);
+            StartGame(Hand.paper, pc);
+        }
+        if(GUI.Button(new Rect(330,10,300,100), "단체전 가위"))
+        {
+            DoTeamPlay(Hand.scissor);
+        }
+        if(GUI.Button(new Rect(330,120,300,100), "단체전 바위"))
+        {
+            DoTeamPlay(Hand.rock);
+        }
+        if(GUI.Button(new Rect(330,230,300,100), "단체전 보"))
+        {
+            DoTeamPlay(Hand.paper);
         }
     }
 
-    void StratGame(Hand hand)
+    void DoTeamPlay(Hand hand)
+    {   
+        int winCount = 0;
+        int loseCount = 0;
+        for(int i=0; i<pcs.Length; i++)
+        {
+            pcs[i] = GetRandomPcHand();
+            GameResult curResult = StartGame(hand, pcs[i]);
+            if(curResult == GameResult.Win) winCount++;
+            if(curResult == GameResult.Lose) loseCount++;
+        }
+        Debug.Log("총 "+pcs.Length+"번의 게임에서 "+winCount+"번 이겼습니다.");
+        Debug.Log(loseCount+"번 졌습니다.");
+
+
+    //foreach 연습
+        // foreach(Hand h in pcs)
+        // {
+        //     Debug.Log(GetHandText(h));
+        // }
+
+    // goto 연습
+        // foreach(var h in pcs)
+        // {
+        //     if(h == Hand.scissor) goto SCISSOR;
+        // }
+
+        // SCISSOR :
+        // Debug.Log("가위");
+    }
+
+    Hand GetRandomPcHand()
     {
-        pc = (Hand)Random.Range(0, (int)Hand.Max);
-        player = hand;
-        Debug.Log("나는 <" + GetHandText(player) + "> 를 냈다.");
-        Debug.Log("컴퓨터는 <" + GetHandText(pc) + "> 를 냈다.");
+        return (Hand)Random.Range(0, (int)Hand.Max);
+    }
+
+    GameResult StartGame(Hand playerHand, Hand pcHand)
+    {
+        // pc = (Hand)Random.Range(0, (int)Hand.Max);
+        player = playerHand;
+
+        Debug.Log("나는 <" + GetHandText(playerHand) + "> 를 냈다.");
+        Debug.Log("컴퓨터는 <" + GetHandText(pcHand) + "> 를 냈다.");
 
         GameResult result;
-        if(pc == player)
+        if(pcHand == playerHand)
         {
             result = GameResult.Draw;
         }
-        else if((pc == Hand.scissor) && (player == Hand.rock)
-            || (pc == Hand.rock) && (player == Hand.paper) 
-            || (pc == Hand.paper) && (player == Hand.scissor))
+        else if((pcHand == Hand.scissor) && (playerHand == Hand.rock)
+            || (pcHand == Hand.rock) && (playerHand == Hand.paper) 
+            || (pcHand == Hand.paper) && (playerHand == Hand.scissor))
         {
             result = GameResult.Win;
         }
@@ -68,6 +120,12 @@ public class rockScissorPaper : MonoBehaviour
             result = GameResult.Lose;
         }
 
+        ShowResult(result);
+        return result;
+    }
+
+    void ShowResult(GameResult result)
+    {
         switch(result)
         {
             case GameResult.Win :
