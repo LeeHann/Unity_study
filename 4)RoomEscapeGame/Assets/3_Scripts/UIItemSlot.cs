f4152bdf77dcd07fd3363e30758d85e9a645bdb6
LeeHann <0104ihn@naver.com>
2020-09-09 11:11:11 +0900

    0909

---
+++ 4)RoomEscapeGame/Assets/3_Scripts/UIItemSlot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIItemSlot : MonoBehaviour
+{
+    Image imgItem;
+
+    public void UpdateSlot(Sprite item)
+    {
+        imgItem = GetComponent<Image>();
+        imgItem.sprite = item;
+
+        //RectTransform rect = GetComponent<RectTransform>(); 
+        RectTransform rect = imgItem.rectTransform; //UI 요소라 이미 rectTransform을 가지고 있음
+
+        rect.sizeDelta = item.textureRect.size; //Vector2 width, height //rectangle : 직사각형
+
+    }
+}
