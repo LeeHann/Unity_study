a77a0edbc8d9ca9a0fb5fde2486c3b744e201edd
LeeHann <0104ihn@naver.com>
2020-09-09 12:17:10 +0900

    0909

--- 4)RoomEscapeGame/Assets/3_Scripts/TitleManager.cs
+++ 4)RoomEscapeGame/Assets/3_Scripts/TitleManager.cs
@@ -8,5 +8,6 @@ public class TitleManager : MonoBehaviour
     public void ClickStart()
     {
         SceneManager.LoadScene("Game");
+        // SceneManager.LoadScene("Game", LoadSceneMode.Additive); //씬을 두개 이상 같이 띄워두고 싶을 때
     }
 }
